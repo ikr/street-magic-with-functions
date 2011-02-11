8ac3e67bc22c61370c673093a4dbf3b74e50e9b0
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-02-12 00:12:12 +0100

    Added the linear iteration demo code

---
+++ js/linear_iteration.js
@@ -0,0 +1,20 @@
+var exptIter = function (product, counter, base, exponent) {
+	return (
+		counter === exponent?
+			product :
+			exptIter(base * product, counter + 1, base, exponent)
+	);
+};
+
+var expt = function (base, exponent) {
+	return exptIter(1, 0, base, exponent);
+};
+
+// (expt 2 4)
+// (expt-iter 1 0 2 4)
+// (expt-iter 2 1 2 4)
+// (expt-iter 4 2 2 4)
+// (expt-iter 8 3 2 4)
+// (expt-iter 16 4 2 4)
+//
+print(expt(2, 4));
