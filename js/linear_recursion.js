0867a692f098721cc23daead7df96431e89231db
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-02-11 22:57:55 +0100

    Linear recursion example done

--- js/linear_recursion.js
+++ js/linear_recursion.js
@@ -1,3 +1,26 @@
-var factorialLoop = function () {
-	
+var Imperative = {
+	exponentiate: function (base, exponent) {
+		var i = 0, result = 1;
+		
+		for (; i < exponent; i++) {
+			result *= base;
+		}
+		
+		return result;
+	}
 };
+ 
+var Functional = {
+	exponentiate: function (base, exponent) {
+		return (exponent <= 0? 1 : (base * this.exponentiate(base, exponent - 1)));
+	}
+};
+
+// expt(2, 4)
+// (* 2 expt(2, 3))
+// (* 2 (* 2 expt(2, 2)))
+// (* 2 (* 2 (* 2 expt(2, 2))))
+// (* 2 (* 2 (* 2 (2 * expt(2, 1)))))
+// (* 2 (* 2 (* 2 (2 * 1))))
+//
+print(Functional.exponentiate(2, 4));
