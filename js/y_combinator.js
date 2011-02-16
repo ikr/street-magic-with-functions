ae5d44870ad176456f5839b5a02ad636bef6ade1
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-02-16 19:39:24 +0100

    started with make_recursive()

--- js/y_combinator.js
+++ js/y_combinator.js
@@ -2,13 +2,69 @@ var magic_factorial = function (n) {
     return (n? n * magic_factorial(n - 1) : 1);
 };
 
-var part_factorial = function (self, n) {
-    return (n? n * self(self, n - 1) : 1);
+// var part_factorial = function (self, n) {
+//     return (n? n * self(self, n - 1) : 1);
+// };
+//
+// var factorial = function (n) {
+//     return part_factorial(part_factorial, n)(n);
+// };
+
+// var part_factorial = function (self) {
+//     return function (n) {
+//         var f = function (x) {
+//             return self(self)(x);
+//         };
+//     
+//         return (n? n * f(n - 1) : 1);
+//     };
+// };
+
+// var part_factorial = function (self) {
+//     var f = function (x) {
+//         return self(self)(x);
+//     };
+//     
+//     return function (f) {    
+//         return function (n) {
+//             return (n? n * f(n - 1) : 1);
+//         };
+//     }(f);
+// };
+
+var almost_factorial = function (f) {
+    return function (n) {
+        return (n? n * f(n - 1) : 1);
+    };
+};
+
+var part_factorial = function (self) {
+    var f = function (x) {
+        return self(self)(x);
+    };
+    
+    return almost_factorial(f);
 };
 
 var factorial = function (n) {
-    return part_factorial(part_factorial, n);
+    return part_factorial(part_factorial)(n);
 };
+
+
+// var make_recursive = function (f) {
+//     return function (n) {
+//         var g = function (x) {
+//             return f(f)(x);
+//         };
+//     
+//         return f(g)(n)
+//     };
+// };
+// 
+// var factorial = function (n) {
+//     return make_recursive(almost_factorial)(n);
+// };
+
 
 print(factorial(5));
 
